9a93110681cec84de4fa128fbf073504b2c6ccd9
yu-i-i <kla9555@gmail.com>
2026-07-30 16:54:36 +0200

    GitLab Synchronization: add gitlabUrl to ExpressLocals; use settings.gitlabSync instead of env vars

--- services/web/modules/gitlab-sync/index.mjs
+++ services/web/modules/gitlab-sync/index.mjs
@@ -7,8 +7,8 @@ if (process.env.GITLAB_SYNC_ENABLED?.toLowerCase() === 'true') {
   logger.debug({}, 'Enabling GitLab Sync module')
 
   // The GITLAB_URL variable has to be set for this module to work, otherwise the module will not be enabled
-  if (process.env.GITLAB_SYNC_URL === undefined || process.env.GITLAB_SYNC_URL === '') {
-    logger.warn({}, 'GitLab Sync module is enabled but GITLAB_SYNC_URL is not set, stopping module initialization')
+  if (!process.env.GITLAB_SYNC_URL || !process.env.GITLAB_SYNC_CLIENT_ID || !process.env.GITLAB_SYNC_CLIENT_SECRET) {
+    logger.warn({}, 'GitLab Sync module is enabled but not all mandatory envirionment variables are set, stopping module initialization')
   } else {
     const [{ default: GitLabSyncRouter },
            { default: SyncStateManager },
@@ -22,9 +22,13 @@ if (process.env.GITLAB_SYNC_ENABLED?.toLowerCase() === 'true') {
   
     const siteUrl = Settings.siteUrl.replace(/\/+$/, '') || 'http://localhost'
     Settings.gitlabSync = {
+      url: process.env.GITLAB_SYNC_URL.replace(/\/+$/, ''),
       clientID: process.env.GITLAB_SYNC_CLIENT_ID,
       clientSecret: process.env.GITLAB_SYNC_CLIENT_SECRET,
       callbackURL: `${siteUrl}/user/gitlab-sync/oauth2/callback`,
+      defaultBranch: process.env.GITLAB_DEFAULT_BRANCH || 'main',
+      mergeRequestPollInterval: process.env.GITLAB_MERGE_REQUEST_POLL_INTERVAL_MS || 1000,
+      mergeRequestTimeout: process.env.GITLAB_MERGE_REQUEST_TIMEOUT_MS || 60_000,
     },
   
     // Delete project sync state from mongo (hook 'projectExpired')
